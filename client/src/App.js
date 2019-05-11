d16e0aded7bf7ce6e29a1aed05c2f15037ab0f1a
Khoa Nguyen <37357006+Khoalabear0424@users.noreply.github.com>
2019-05-11 13:46:33 -0700

    made state into objets

--- client/src/App.js
+++ client/src/App.js
@@ -5,16 +5,39 @@ import './App.css';
 
 class App extends Component {
   state = {
-    imgLinks: [
-      "https://www.petmd.com/sites/default/files/Acute-Dog-Diarrhea-47066074.jpg",
-      "https://www.broadsheet.com.au/media/cache/6c/8b/6c8b1577b91215610644d49777e3ee63.jpg",
-      "https://www.animalhouseshelter.com/wp-content/uploads/2019/05/Ace-1-600x600.png",
-      "https://fortunedotcom.files.wordpress.com/2019/01/boo.jpg",
-      "https://pbs.twimg.com/profile_images/446566229210181632/2IeTff-V.jpeg",
-      "https://i.pinimg.com/originals/45/de/3a/45de3af40bc566be49b31e1b69badc3d.jpg",
-      "https://i.pinimg.com/originals/21/7d/cd/217dcd4712168fe88ed954649f5c4b74.jpg",
-      "https://i.pinimg.com/originals/af/6b/0c/af6b0cf1fc2c1711d49440f09530e7ab.jpg"
-    ]
+    imgLinks: [{
+      select: false,
+      src: "https://www.petmd.com/sites/default/files/Acute-Dog-Diarrhea-47066074.jpg"
+    },
+    {
+      select: false,
+      src: "https://www.broadsheet.com.au/media/cache/6c/8b/6c8b1577b91215610644d49777e3ee63.jpg"
+    },
+    {
+      select: false,
+      src: "https://www.animalhouseshelter.com/wp-content/uploads/2019/05/Ace-1-600x600.png"
+    },
+    {
+      select: false,
+      src: "https://fortunedotcom.files.wordpress.com/2019/01/boo.jpg"
+    },
+    {
+      select: false,
+      src: "https://pbs.twimg.com/profile_images/446566229210181632/2IeTff-V.jpeg"
+    },
+    {
+      select: false,
+      src: "https://i.pinimg.com/originals/45/de/3a/45de3af40bc566be49b31e1b69badc3d.jpg"
+    },
+    {
+      select: false,
+      src: "https://i.pinimg.com/originals/21/7d/cd/217dcd4712168fe88ed954649f5c4b74.jpg"
+    },
+    {
+      select: false,
+      src: "https://i.pinimg.com/originals/af/6b/0c/af6b0cf1fc2c1711d49440f09530e7ab.jpg"
+    }
+    ],
   }
 
   click = (id) => {
@@ -28,9 +51,10 @@ class App extends Component {
         <div className="container imageDisplay">
           {this.state.imgLinks.map((imgLink, index) =>
             <ImageBlock
+              key={index}
               id={index}
               click={this.click}
-              imgLinkSrc={imgLink}
+              imgLinkSrc={imgLink.src}
             />
           )}</div>
       </div>
